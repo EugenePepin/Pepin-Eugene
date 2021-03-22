75763b76050b495f91a083383f0cf3855beb1366
EugenePepin <78425689+EugenePepin@users.noreply.github.com>
2021-03-23 00:12:22 +0200

    Add files via upload

---
+++ Task2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h> 
+int main() {
+setlocale (LC_CTYPE,"ua");
+float y;
+const float a = 2;
+const float b = 10;
+const float c = 20;
+printf("\n Обчислення функцiї з другого завдання 16 варiанту. \n");
+y = (((a+b*c)/(2*a-b-c))+((a)/(b+a)));
+printf("\n Вiдповiдь = ");
+printf("%f",y);
+printf("\n");
+printf("\n");
+return 0;
+}
