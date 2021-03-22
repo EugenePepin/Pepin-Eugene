1b397979cff679cd586cc9be0bdfa6d3cd00a8dd
EugenePepin <78425689+EugenePepin@users.noreply.github.com>
2021-03-23 00:14:27 +0200

    Update and rename Tsk3.c to Task3.c

---
+++ Task3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h> 
+int main() {
+	setlocale (LC_CTYPE,"ua");
+       int x,z;
+	float y;
+	printf("\n Обчислення функцiї з третього завдання 16 варiанту. \n");
+        printf("\n Число x=");
+        scanf("%d",&x); 
+        printf("\n Число z=");
+	scanf("%d",&z); 
+        if (x>=0) {
+		printf ("\n В данному випадку x має бути менше за нуль.  \n");
+	}
+	y = sqrt(log10(1-2*x)/(z+3));
+       printf("\n Вiдповiдь =  ");
+       printf("%f",y); 
+       printf("\n");
+	printf("\n");
+       return 0;
+}
+
